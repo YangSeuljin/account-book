7b78b866e1e7b2e53a62eb275583731967e370ad
YangSeuljin <gluestuck@naver.com>
2023-01-18 16:24:00 +0900

    - http url filter 접근 규칙 수정

--- src/main/java/com/accountbook/config/SecurityConfig.java
+++ src/main/java/com/accountbook/config/SecurityConfig.java
@@ -1,36 +1,49 @@
 package com.accountbook.config;
 
+import com.accountbook.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private UserService userService;
+    @Value("${jwt.secret-key}")
+    private String key;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().loginPage("/members/login") //로그인 페이지 URL 설정
+        http.formLogin()
+                .loginPage("/login") //로그인 페이지 URL 설정
                 .defaultSuccessUrl("/list") //로그인 성공시 이동할 URL
                 .failureUrl("/members/login/error")
                 .and()
+                .addFilterBefore(new JwtTokenFilter(key,userService), UsernamePasswordAuthenticationFilter.class)
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                 .logoutSuccessUrl("/members/login");
 
-        http.authorizeRequests().mvcMatchers("/", "/members/**", "/book/**", "/images/**")
+        http.authorizeRequests().mvcMatchers("/","/join", "/members/**", "/book/**", "/images/**")
                 .permitAll();
     }
 
     //static 디렉터리의 하위 파일은 인증을 무시하도록 설정.
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**","/members/join");
     }
 
     @Bean
